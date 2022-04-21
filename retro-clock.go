f22d1c4bcef5d1c3ccb1de62b244c2d15c8e1954
vlkan <thkvolkanonder@gmail.com>
2022-04-21 16:03:14 +0300

    update screen

--- retro-clock.go
+++ retro-clock.go
@@ -1,9 +1,21 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/inancgumus/screen"
+)
 
 func main() {
-	// for keeping things easy to read and type-safe
 	type placeholder [5]string
 
 	zero := placeholder{
@@ -86,32 +98,46 @@ func main() {
 		"███",
 	}
 
-	// This array's type is "like": [10][5]string
-	//
-	// However:
-	// + "placeholder" is not equal to [5]string in type-wise.
-	// + Because: "placeholder" is a defined type, which is different
-	//   from [5]string type.
-	// + [5]string is an unnamed type.
-	// + placeholder is a named type.
-	// + The underlying type of [5]string and placeholder is the same:
-	//     [5]string
+	colon := placeholder{
+		"   ",
+		" ░ ",
+		"   ",
+		" ░ ",
+		"   ",
+	}
+
 	digits := [...]placeholder{
 		zero, one, two, three, four, five, six, seven, eight, nine,
 	}
 
-	// Explanation: digits[0]
-	// + Each element of clock has the same length.
-	// + So: Getting the length of only one element is OK.
-	// + This could be: "zero" or "one" and so on... Instead of: digits[0]
-	//
-	// The range clause below is ~equal to the following code:
-	// line := 0; line < 5; line++
-	for line := range digits[0] {
-		// Print a line for each placeholder in digits
-		for digit := range digits {
-			fmt.Print(digits[digit][line], "  ")
+	screen.Clear()
+
+	for {
+		screen.MoveTopLeft()
+
+		now := time.Now()
+		hour, min, sec := now.Hour(), now.Minute(), now.Second()
+
+		clock := [...]placeholder{
+			digits[hour/10], digits[hour%10],
+			colon,
+			digits[min/10], digits[min%10],
+			colon,
+			digits[sec/10], digits[sec%10],
 		}
-		fmt.Println()
+
+		for line := range clock[0] {
+			for index, digit := range clock {
+				// colon blink
+				next := clock[index][line]
+				if digit == colon && sec%2 == 0 {
+					next = "   "
+				}
+				fmt.Print(next, "  ")
+			}
+			fmt.Println()
+		}
+
+		time.Sleep(time.Second)
 	}
 }
